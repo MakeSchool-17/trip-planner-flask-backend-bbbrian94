a7bf45238125a2916564bdacfb68d67a4a60cf5c
王 新沿 <wangxinyan0619@0587375184.wireless.umich.net>
2015-11-16 21:55:12 -0500

    new files

--- server.py
+++ server.py
@@ -3,31 +3,62 @@ from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from utils.mongo_json_encoder import JSONEncoder
+from flask.ext.bcrypt import bcrypt
+from functools import wraps
 
 # Basic Setup
 app = Flask(__name__)
 mongo = MongoClient('localhost', 27017)
 app.db = mongo.develop_database
 api = Api(app)
+app.bcrypt_rounds = 12
+
+
+def hash_password(password, salt):
+    encoded = password.encode('utf-8')
+    return bcrypt.hashpw(encoded, salt)
+
+
+def check_auth(username, password):
+    # retrieve api pw key from database.
+    user = app.db.users.find_one({"name": username})
+    if user is None:
+        return False
+    password_hash = user['password']
+    password_encrypted = hash_password(password, salt=password_hash)
+    return password_encrypted == password_hash
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            message = {"error": "Basic Auth Required."}
+            resp = jsonify(message)
+            resp.status_code = 401
+            return resp
+        return f(*args, **kwargs)
+    return decorated
 
 
 # Implement REST Resource
+# [Ben-G] Example breakpoints can be removed once you've implemeted the trip API
 class MyObject(Resource):
-
+    
     def post(self):
         new_myobject = request.json
         myobject_collection = app.db.myobjects
         result = myobject_collection.insert_one(request.json)
-
-        myobject = myobject_collection.find_one(
-            {"_id": ObjectId(result.inserted_id)})
-
+        
+        myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        
         return myobject
-
+    
     def get(self, myobject_id):
         myobject_collection = app.db.myobjects
         myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
-
+        
         if myobject is None:
             response = jsonify(data=[])
             response.status_code = 404
@@ -36,11 +67,126 @@ class MyObject(Resource):
             return myobject
 
 
+class Trip(Resource):
+    
+    @requires_auth
+    def post(self):
+        trip_collection = app.db.trips
+        request.json['owner'] = request.authorization['username']
+        result = trip_collection.insert_one(request.json)
+        trip = trip_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        return trip
+    
+    @requires_auth
+    def get(self, trip_id=None):
+        trip_collection = app.db.trips
+        if trip_id is None:
+            trips = trip_collection.find()
+            users_trips = []
+            for each_trip in trips:
+                if each_trip['owner'] == request.authorization['username']:
+                    users_trips.append(each_trip)
+            return users_trips
+        else:
+            trip = trip_collection.find_one({"_id": ObjectId(trip_id)})
+            if trip is None:
+                response = jsonify(data=[])
+                response.status_code = 404
+                return response
+            elif trip['owner'] != request.authorization['username']:
+                response = jsonify(data=[])
+                response.status_code = 401
+                return response
+            else:
+                return trip
+
+# put_request from benji:
+    @requires_auth
+    def put(self, trip_id):
+        new_trip = request.json
+        new_trip['user'] = request.authorization.username
+        trip_collection = app.db.trips
+        
+        # remove _id since we can't update it and would need to
+        # transform it into an ObjectId
+        del request.json['_id']
+        trip_collection.update_one({'_id': ObjectId(trip_id),
+                                   'owner': request.authorization.username},
+                                   {'$set': request.json})
+        trip = trip_collection.find_one(ObjectId(trip_id))
+        return trip
+
+    @requires_auth
+    def delete(self, trip_id):
+        trip_collection = app.db.trips
+        trip = trip_collection.find_one({"_id": ObjectId(trip_id)})
+        if trip is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        else:
+            trip_collection.delete_one({"_id": ObjectId(trip_id)})
+            return trip_collection.find_one({"_id": ObjectId(trip_id)})
+
+
+class User(Resource):
+    
+    # def get_all(self, trip_ids):
+    #     trips = []
+    #     responses = []
+    #     for each_id in trip_ids:
+    #         each_trip = trip_collection.find_one({"_id": ObjectId(each_id)})
+    #         trips.append(each_trip)
+    #         if each_trip is None:
+    #             response = jsonify(data=[])
+    #             response.status_code = 404
+    #             responses.append(response)
+    #     return trips
+    # def __init__(self):
+    #     self.trips = []
+    #     self.name = None
+    #     self.password_hash = None
+    
+    def post(self):
+        user_collection = app.db.users
+        password = request.json["password"]
+        pass_hash = hash_password(password, bcrypt.gensalt(app.bcrypt_rounds))
+        request.json["password"] = pass_hash
+        request.json["trips"] = []
+        
+        result = user_collection.insert_one(request.json)
+        user = user_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        del user['password']  # DO NOT return password back to the client!!!
+        return user
+    
+    @requires_auth
+    def get(self, user_id):
+        user_collection = app.db.users
+        user = user_collection.find_one({"_id": ObjectId(user_id)})
+        if user is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        del user['password']  # DO NOT return password back to the client!!!
+        return user
+    
+    def put(self, user_id):
+        user_collection = app.db.users
+        user = user_collection.find_one({"_id": ObjectId(user_id)})
+        if trip is None:
+            response = jsonify(data=[])
+            response.status_code = 404
+            return response
+        result = user_collection.update_one(request.json)
+        return result
+
 # Add REST resource to API
 api.add_resource(MyObject, '/myobject/', '/myobject/<string:myobject_id>')
-
-
+api.add_resource(Trip, '/trip/', '/trip/<string:trip_id>')
+api.add_resource(User, '/user/', '/user/<string:user_id>')
 # provide a custom JSON serializer for flaks_restful
+
+
 @api.representation('application/json')
 def output_json(data, code, headers=None):
     resp = make_response(JSONEncoder().encode(data), code)
@@ -48,7 +194,7 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailed information
-    # about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
+    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
+# check_auth("test", "testpw")
--- tests.py
+++ tests.py
@@ -2,55 +2,219 @@ import server
 import unittest
 import json
 from pymongo import MongoClient
+from base64 import b64encode
+
+
+# [Ben-G] Nice job extracting this into a separate function!
+def create_auth_header(correct, username):
+    if correct == True:
+        password = "test"
+    else:
+        password = "testwrong"
+    pw_str = "{0}:{1}".format(username, password)
+    # encoded = pw_str.encode("utf-8")
+    # encoded = str.encode(pw_str)
+
+    auth = 'Basic ' + b64encode(pw_str.encode('utf-8')).decode('utf-8')
+    return {"Authorization": auth}
+# resulting header: "Authorization: Basic TmV3VXNlcjp0ZXN0"
 
 
 class FlaskrTestCase(unittest.TestCase):
-
+    
     def setUp(self):
         self.app = server.app.test_client()
         # Run app in testing mode to retrieve exceptions and stack traces
         server.app.config['TESTING'] = True
-
+        
         # Inject test database into application
         mongo = MongoClient('localhost', 27017)
         db = mongo.test_database
         server.app.db = db
-
+        
         # Drop collection (significantly faster than dropping entire db)
         db.drop_collection('myobjects')
-
-        # MyObject tests
-
+        db.drop_collection('trips')
+        db.drop_collection('users')
+    
+    # MyObject tests
+    
+    # [Ben-G] Once you've implemented the Trip API you can remove the tests for these example endpoints
     def test_posting_myobject(self):
-        response = self.app.post(
-            '/myobject/',
-            data=json.dumps(dict(name="A object")),
-            content_type='application/json')
-
+        response = self.app.post('/myobject/',data=json.dumps(dict(name="A object")),content_type='application/json')
+            
         responseJSON = json.loads(response.data.decode())
-
+                                 
         self.assertEqual(response.status_code, 200)
         assert 'application/json' in response.content_type
         assert 'A object' in responseJSON["name"]
 
-    def test_getting_object(self):
-        response = self.app.post(
-            '/myobject/',
-            data=json.dumps(dict(name="Another object")),
-            content_type='application/json')
-
+    def test_getting_obj(self):
+        response = self.app.post('/myobject/',data=json.dumps(dict(name="Another object")),content_type=    'application/json')
         postResponseJSON = json.loads(response.data.decode())
         postedObjectID = postResponseJSON["_id"]
-
+        
         response = self.app.get('/myobject/' + postedObjectID)
+        responseJSON = json.loads(response.data.decode())
+        
+        self.assertEqual(response.status_code, 200)
+        assert 'Another object' in responseJSON["name"]
+            
+    def test_getting_non_existent_obj(self):
+        response = self.app.get('/myobject/55f0cbb4236f44b7f0e3cb23')
+        self.assertEqual(response.status_code, 404)
+            
+
+    # Trip tests
+    def test_getting_trip(self):
+        response = self.app.post('/user/',
+                                 data=json.dumps(dict(
+                                                      name="NewUser",
+                                                      password="test",
+                                                      )),
+                                 content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+                                     
+        response = self.app.post('/trip/',data=json.dumps(dict(name="Another object")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+                                                 
+        response = self.app.get('/trip/' + postedObjectID, headers=create_auth_header(True, "NewUser"))
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+
+
+# [Ben-G] You should add assertions to your test case! Without assertions
+# this code doesn't test anything and will never fail
+
+    def test_getting_non_existent_trip(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        
+        response = self.app.get('/trip/55f0cbb4236f44b7f0e3cb23', headers=create_auth_header(True, "NewUser"))
+        self.assertEqual(response.status_code, 404)
+            
+    def test_getting_other_users_trip(self):
+        # create user1
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+            
+        # create user2
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser2",password="test",)),content_type='application/json')
+                                 
+        # create trip for user1
+        response = self.app.post('/trip/',data=json.dumps(dict(name="Another object")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+                                             
+        # attempt to access user1's trip for user2
+        response = self.app.get('/trip/' + postedObjectID, headers=create_auth_header(True, "NewUser2"))
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 401)
+
+    def test_get_many_trips(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        
+        response = self.app.post('/trip/',data=json.dumps(dict(name="First Trip")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+            
+        response = self.app.post('/trip/',data=json.dumps(dict(name="Second Trip")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+                                 
+        response = self.app.get('/trip/', headers=create_auth_header(True, "NewUser"))
+        responseJSON = json.loads(response.data.decode())
+        assert len(responseJSON) == 2
+        assert "First Trip" in responseJSON[0]['name']
+        assert "Second Trip" in responseJSON[1]['name']
+        self.assertEqual(response.status_code, 200)
+
+# [Ben-G] I'm assuming this is still in the works, but later this request
+# should require an authorization header
+    def test_posting_trip(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        
+        response = self.app.post('/trip/',data=json.dumps(dict(name="A trip")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+            
+        responseJSON = json.loads(response.data.decode())
+                                 
+        self.assertEqual(response.status_code, 200)
+        assert 'application/json' in response.content_type
+        assert 'A trip' in responseJSON["name"]
+
+# test case from Benji's code:
+    def test_unauthorized_posting_trip(self):
+        response = self.app.post('/trip/',
+                             data=json.dumps(dict(
+                                                  name="Stuttgart Roadtrip"
+                                                  )),
+                             content_type='application/json')
+        self.assertEqual(response.status_code, 401)
+            
+    def test_modifying_trip(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+
+        postResponseJSON = json.loads(response.data.decode())
+
+        response = self.app.post('/trip/',headers=create_auth_header(True, "NewUser"),data=json.dumps(dict(name="A Trip")),content_type='application/json')
+                                     
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+                                             
+        # code from Benji, for put call:
+        response = self.app.put('/trip/' + postedObjectID, headers=create_auth_header(True, "NewUser"), data=json.dumps(dict(name="San Francisco Roadtrip",_id=postedObjectID)),content_type='application/json')
+
         responseJSON = json.loads(response.data.decode())
 
         self.assertEqual(response.status_code, 200)
-        assert 'Another object' in responseJSON["name"]
-
-    def test_getting_non_existent_object(self):
-        response = self.app.get('/myobject/55f0cbb4236f44b7f0e3cb23')
+        assert "San Francisco" in responseJSON["name"]
+                                                     
+    def test_deleting_trip(self):
+    # [Ben-G] I'm assuming this is still in the works, but later this request
+    # should require an authorization header
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+        
+        response = self.app.post('/trip/',data=json.dumps(dict(name="ToBeDeletedTrip")),headers=create_auth_header(True, "NewUser"),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+        response = self.app.delete('/trip/' + postedObjectID, headers=create_auth_header(True, "NewUser"))
+        self.assertEqual(response.status_code, 200)
+        response = self.app.get('/trip/' + postedObjectID, headers=create_auth_header(True, "NewUser"))
+        responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 404)
+                             # [Ben-G] A good way for testing successful deletion would be to issue a GET
+# request to see if you can retrieve the deleted trip and expect the server
+# to return a 404 (Not Found) status code
+
+# User tests
+    def test_signup_user(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        
+        responseJSON = json.loads(response.data.decode())
+        assert 'application/json' in response.content_type
+        assert 'NewUser' in responseJSON["name"]
+        assert 'password' not in responseJSON
+        self.assertEqual(response.status_code, 200)
+                             
+                             # [Ben-G] Would be better to name this 'test_valid_credentials' since the API doesn't
+                             # actually support the concept of login/logout
+    def test_valid_credentials(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+                                     
+        response = self.app.get('/user/' + postedObjectID,headers=create_auth_header(True, "NewUser"))
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+                                         
+    def test_verify_credentials_wrong(self):
+        response = self.app.post('/user/',data=json.dumps(dict(name="NewUser",password="test",)),content_type='application/json')
+        postResponseJSON = json.loads(response.data.decode())
+        postedObjectID = postResponseJSON["_id"]
+        response = self.app.get('/user/' + postedObjectID,headers=create_auth_header(False, "NewUser"))
+        self.assertEqual(response.status_code, 401)
+
+# def test_create_trip(self):
+#     pass
 
 if __name__ == '__main__':
     unittest.main()
